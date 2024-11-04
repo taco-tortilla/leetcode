3a3dbf9925c9759064b39304ac057f80244b3891
taco-tortilla <tortillataco0@gmail.com>
2024-11-04 13:40:12 +0900

    fix: func name

--- easy/9_palindrome_number/test_solution.py
+++ easy/9_palindrome_number/test_solution.py
@@ -6,7 +6,7 @@ from solution import Solution
     "value, expected",
     [(121, True), (-121, False), (10, False)],
 )
-def test_(value, expected):
+def test_isPalindrome(value, expected):
     solution = Solution()
     result = solution.isPalindrome(value)
     assert result == expected, f"Expected {expected}, got {result}"
